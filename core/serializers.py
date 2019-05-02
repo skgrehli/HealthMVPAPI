80dea076c42d1ef0bb4fe50a548d0acf98d074cb
Sandeep Goswami <sandeepgoswami108@gmail.com>
2019-05-02 17:53:01 +0530

    Update apu

--- core/serializers.py
+++ core/serializers.py
@@ -29,9 +29,9 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
     @transaction.atomic()
     def create(self, validated_data):
-        username = validated_data['username']
-        first_name = validated_data['first_name']
-        last_name = validated_data['last_name']
+        username = validated_data.get('username','')
+        first_name = validated_data.get('first_name')
+        last_name = validated_data.get('last_name')
         email = validated_data['email']
         user = User.objects.create(**validated_data)
         user.set_password(validated_data['password'])
